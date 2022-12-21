450c2967e7a9fe8a124fa703d99dfa7512997e57
Aivan Monceller <aivan@monceller.com>
2022-12-21 23:40:07 +0800

    feat(metadata): read metadata from image being sent

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,7 +1,10 @@
+import datetime
+import os
 import discord
 import config
 from discord import app_commands
 from discord.ext import commands
+from PIL import Image, PngImagePlugin
 
 class Hello(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,9 +14,60 @@ class Hello(commands.Cog):
     async def on_ready(self):
         print('Hello cog loaded.')
 
+    # watch on_message event
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # check if the message is from the bot
+        if message.author == self.bot.user:
+            return
+        print(f"Message from {message.author}: {message.content}")
+        # check if the message contains any attachments
+        if message.attachments:
+            # get the first attachment
+            attachment = message.attachments[0]
+            print(f"Attachment: {attachment.filename}")
+            # check if the attachment is a PNG image
+            if attachment.filename.endswith(".png"):
+                print("Image is a PNG file.")
+                # create the images folder if it does not exist
+                if not os.path.exists("images"):
+                    os.makedirs("images")
+
+                # generate a unique filename for the image
+                timestamp = datetime.datetime.now().timestamp()
+                image_name = f"image{timestamp}.png"
+                image_path = f"images/{image_name}"
+
+                print(f"Saving image as {image_name}")
+
+                # save the image to the images folder
+                await attachment.save(image_path)
+
+                print(f"Image saved as {image_name}")
+
+                # open the image and get the metadata
+                with Image.open(image_path) as im:
+                    metadata = im.info
+
+                print(f"Image metadata: {metadata}")
+                
+                # delete the image
+                os.remove(image_path)
+
+                # send the metadata back to the server
+                await message.channel.send(f"Image metadata: {metadata}", reference=message)
+            else:
+                await message.channel.send("Image is not a PNG file.")
+
     @commands.command()
     async def sync(self, ctx) -> None:
-        fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+        # check if the user is the owner of the bot
+        if ctx.author.id == ctx.bot.owner_id:
+            # send a message to the channel
+            print("You are not allowed!")
+            return
+        # fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+        fmt = await ctx.bot.tree.sync()
         await ctx.send(
           f"Synced {len(fmt)} commands to the current guild."
         )
@@ -59,7 +113,9 @@ class Hello(commands.Cog):
     ])
     #end new code
     async def choosecolor(self, interaction: discord.Interaction, colors: discord.app_commands.Choice[int]):
+        print("in choosecolor")
         await interaction.response.send_message(f'Hey {interaction.user.name} you chose color {colors.name}')
 
 async def setup(bot):
-    await bot.add_cog(Hello(bot), guilds=[discord.Object(id=config.GUILD)])
+    # await bot.add_cog(Hello(bot), guilds=[discord.Object(id=config.GUILD)])
+    await bot.add_cog(Hello(bot))
